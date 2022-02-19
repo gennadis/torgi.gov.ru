8b3a279e5b21069e6e524e01e736613ee458f492
Gennady Badinov <gennady.badinov@gmail.com>
2022-02-19 20:26:23 +0300

    Fetch opendata passport

--- torgi.py
+++ torgi.py
@@ -1,7 +1,10 @@
 import datetime
+import json
 
+import requests
 
 OPENDATA_PASSPORT_URL = "https://torgi.gov.ru/new/opendata/7710568760-notice/data-{}T0000-{}T0000-structure-20220101.json"
+PASSPORT_FILENAME = "passport.json"
 
 
 def get_opendata_passport_url(days_count: int) -> str:
@@ -14,9 +17,26 @@ def get_opendata_passport_url(days_count: int) -> str:
     return passport_url
 
 
+def fetch_opendata_passport(url: str, verify_ssl: bool, filename: str) -> str:
+    response = requests.get(url, verify=verify_ssl)
+    response.raise_for_status()
+
+    with open(filename, "w") as file:
+        json.dump(response.json(), file, indent=2)
+
+    return filename
+
+
 def main():
     passport_url = get_opendata_passport_url(days_count=1)
-    print(passport_url)
+    try:
+        passport_json = fetch_opendata_passport(
+            url=passport_url, verify_ssl=True, filename=PASSPORT_FILENAME
+        )
+    except requests.exceptions.SSLError:
+        passport_json = fetch_opendata_passport(
+            url=passport_url, verify_ssl=False, filename=PASSPORT_FILENAME
+        )
 
 
 if __name__ == "__main__":
