bbca0f4cf01a55e823822d3bb091bc67954f15f4
Gennady Badinov <gennady.badinov@gmail.com>
2022-02-20 21:15:12 +0300

    Filter notifications by region

--- mongo_db_client.py
+++ mongo_db_client.py
@@ -1,3 +1,7 @@
+import os
+from pprint import pprint
+
+from dotenv import load_dotenv
 from pymongo import MongoClient
 
 
@@ -11,3 +15,32 @@ def save_notification(
 ) -> None:
     collection = client[collection_name]
     collection.insert_one(notification)
+
+
+def filter_by_region(db: MongoClient, region_code: str):
+    region_notifications = db["NOTIFICATIONS"].find(
+        {"structuredObject.notice.lots.biddingObjectInfo.subjectRF.code": region_code}
+    )
+    apartments = db["NOTIFICATIONS"].find(
+        {"structuredObject.notice.lots.biddingObjectInfo.category.code": "9"}
+    )
+
+    return region_notifications
+
+
+def main():
+    load_dotenv()
+    mongodb_url = os.getenv("MONGODB_URL")
+
+    db = get_database(mongodb_url)
+
+    moscow_notifications = filter_by_region(db, "77")
+    for notification in list(moscow_notifications):
+        lots = notification["structuredObject"]["notice"]["lots"]
+        for lot in lots:
+            pprint(lot["lotDescription"])
+        print("-------------")
+
+
+if __name__ == "__main__":
+    main()
