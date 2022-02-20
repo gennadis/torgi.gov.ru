ee1b024a93ef48891b6f9de454b1053981f9d959
Gennady Badinov <gennady.badinov@gmail.com>
2022-02-20 17:51:24 +0300

    Refactor notification dates to fetch

--- torgi.py
+++ torgi.py
@@ -12,16 +12,21 @@ from mongo_db_client import save_notice, get_database
 
 
 OPENDATA_PASSPORT_URL = "https://torgi.gov.ru/new/opendata/7710568760-notice/data-{}T0000-{}T0000-structure-20220101.json"
-PASSPORT_FILENAME = "passport_{}.json"
-NOTIFICATIONS_DIRNAME = "notifications_{}"
+PASSPORT_FILENAME = "passport_{}_{}.json"
+NOTIFICATIONS_DIRNAME = "notifications"
+DAYS_DELTA = 1
 
 
-def get_opendata_passport_url(days_count: int) -> str:
+def get_dates(days_count: int) -> list:
     today = datetime.date.today()
     past_day = today - datetime.timedelta(days=days_count)
     dates = [date.strftime("%Y%m%d") for date in (past_day, today)]
 
-    passport_url = OPENDATA_PASSPORT_URL.format(*dates)
+    return dates
+
+
+def get_opendata_passport_url(past_date: str, current_date: str) -> str:
+    passport_url = OPENDATA_PASSPORT_URL.format(past_date, current_date)
 
     return passport_url
 
@@ -49,7 +54,7 @@ def fetch_notification(url: str):
         json.dump(notification, file, ensure_ascii=False, indent=2)
 
 
-def fetch_all_notifications(passport: dict) -> list:
+def fetch_all_notifications(passport: dict):
     notification_urls = [
         notification["href"]
         for notification in passport["listObjects"]
@@ -83,8 +88,11 @@ def main():
     os.makedirs("notifications", exist_ok=True)
     urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
-    passport_url = get_opendata_passport_url(days_count=1)
-    passport = fetch_opendata_passport(url=passport_url, filename=PASSPORT_FILENAME)
+    yesterday, today = get_dates(DAYS_DELTA)
+    passport_url = get_opendata_passport_url(past_date=yesterday, current_date=today)
+    passport = fetch_opendata_passport(
+        url=passport_url, filename=PASSPORT_FILENAME.format(yesterday, today)
+    )
 
     fetch_all_notifications(passport)
 
