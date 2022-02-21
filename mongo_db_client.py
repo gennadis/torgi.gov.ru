ede58ea33de743884e82df2eb8776bc9d6f217ba
Gennady Badinov <gennady.badinov@gmail.com>
2022-02-21 04:21:42 +0300

    Filter notifications by region and category type

--- mongo_db_client.py
+++ mongo_db_client.py
@@ -1,8 +1,12 @@
+import locale
 import os
 from pprint import pprint
 
 from dotenv import load_dotenv
 from pymongo import MongoClient
+
+MOSCOW_REGION_CODE = "77"
+APARTAMENTS_CODE = "9"
 
 
 def get_database(mongodb_url: str) -> MongoClient:
@@ -17,28 +21,35 @@ def save_notification(
     collection.insert_one(notification)
 
 
-def filter_by_region(db: MongoClient, region_code: str):
+def filter_notifications(db: MongoClient, region_code: str, category_code: str):
     region_notifications = db["NOTIFICATIONS"].find(
-        {"structuredObject.notice.lots.biddingObjectInfo.subjectRF.code": region_code}
-    )
-    apartments = db["NOTIFICATIONS"].find(
-        {"structuredObject.notice.lots.biddingObjectInfo.category.code": "9"}
+        {
+            "structuredObject.notice.lots.biddingObjectInfo.subjectRF.code": region_code,
+            "structuredObject.notice.lots.biddingObjectInfo.category.code": category_code,
+        }
     )
 
     return region_notifications
 
 
 def main():
+    locale.setlocale(locale.LC_ALL, "ru_RU")
+
     load_dotenv()
     mongodb_url = os.getenv("MONGODB_URL")
 
     db = get_database(mongodb_url)
 
-    moscow_notifications = filter_by_region(db, "77")
+    moscow_notifications = filter_notifications(
+        db, region_code=MOSCOW_REGION_CODE, category_code=APARTAMENTS_CODE
+    )
     for notification in list(moscow_notifications):
+        print(notification["structuredObject"]["notice"]["commonInfo"]["href"])
         lots = notification["structuredObject"]["notice"]["lots"]
         for lot in lots:
-            pprint(lot["lotDescription"])
+            price = locale.currency(float(lot["priceMin"]), grouping=True)
+            print(price)
+            print(lot["lotDescription"])
         print("-------------")
 
 
