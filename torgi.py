1f26f18d6f8358e46c808b63964aabd1ad463181
Gennady Badinov <gennady.badinov@gmail.com>
2022-02-21 09:44:19 +0300

    Save each passport to database

--- torgi.py
+++ torgi.py
@@ -8,7 +8,7 @@ import requests
 from dotenv import load_dotenv
 from tqdm import tqdm
 
-from mongo_db_client import get_database, save_notification
+from mongo_db_client import get_database, save_to_database
 
 
 OPENDATA_PASSPORT_URL = "https://torgi.gov.ru/new/opendata/7710568760-notice/data-{}T0000-{}T0000-structure-20220101.json"
@@ -16,6 +16,7 @@ NOTIFICATIONS_DIRPATH = "notifications/{}_{}/"
 PASSPORT_FILEPATH = "notifications/passport.json"
 DAYS_DELTA = 1
 MONGODB_COLLECTION_NAME = "MOSCOW_APARTAMENTS"
+PASSPORTS_COLLECTION_NAME = "PASSPORTS"
 MOSCOW_REGION_CODE = "77"
 APARTAMENTS_CODE = "9"
 OFFICES_CODE = "11"
@@ -91,7 +92,7 @@ def main():
     urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
     # yesterday, today = get_dates(DAYS_DELTA)
-    yesterday, today = "20220218", "20220219"
+    yesterday, today = "20220217", "20220218"
 
     notifications_dirpath = NOTIFICATIONS_DIRPATH.format(yesterday, today)
     os.makedirs(notifications_dirpath, exist_ok=True)
@@ -106,7 +107,9 @@ def main():
         filepath = os.path.join(notifications_dirpath, filename)
         with open(filepath, "r") as file:
             notification = json.load(file)
-            save_notification(mongodb_client, MONGODB_COLLECTION_NAME, notification)
+            save_to_database(mongodb_client, MONGODB_COLLECTION_NAME, notification)
+
+    save_to_database(mongodb_client, PASSPORTS_COLLECTION_NAME, passport)
     os.remove(PASSPORT_FILEPATH)
 
 
